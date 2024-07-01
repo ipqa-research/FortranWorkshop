ca77654b877bd2064977e3d14a9be479b99c675e
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-01 08:52:32 -0300

    evaluators

--- tarea/dia1.md
+++ tarea/dia1.md
@@ -1,5 +1,7 @@
 # Actividades del día 1
 
+## Modulos
+
 1. Crear un módulo llamado `dia_1_constants` que guarde las constantes:
    - `R = 0.082`.
    - `pr` que corresponda a la selección de doble precisión.
@@ -10,3 +12,26 @@
      y que empiece con un valor predeterminado de `5`. 
    - Una subroutina `get_x(x)` que asigne el valor de la variable `x` del módulo
      a la variable `x` de salida de la rutina.
+
+5. Finalmente, generar un módulo `dia_1` que junte a todos los módulos
+   definidos.
+
+## Subroutinas
+
+1. Escribir una subroutina `sumar`, que tome dos argumentos `x`, e `y` y
+   devuelva un tercer argumento `z` corresponidente a la suma entre ambos.
+   `y` debe ser un argumento opcional, si no está presente se debe hacer la 
+   suma de `x` consigo misma.
+
+2. Escribir una subroutina `sumarr` que tome un array `x` de tamaño asumido y
+   que devuelva una variable `y` correspondiente a la suma de todos los
+   elementos de la misma.
+
+3. Definir otra subrutina `sumat` donde en lugar de un array sea una matriz,
+   e implementar una interface `suma` que abarque a ambas.
+
+## Allocatables
+
+## Namelist
+
+## Tipos derivados
--- test/dia1.f90
+++ test/dia1.f90
@@ -1,27 +1,56 @@
 program dia_1_evaluation
+    use fortran_workshop_testing, only: assert, test
+    use dia1_modules, only: d1_mods => run_tests
     implicit none
-    call modules_constants
+
+    call d1_mods
+
+    call subroutines_sumar
+    call subroutines_sumarr
+    call subroutines_sumat
 
 contains
 
-    subroutine print_test(str)
-        character(len=*), intent(in) :: str
-        print *, str
+    subroutine subroutines_sumar
+        use dia_1, only: pr, sumar
+        real(pr) :: x, y, z
+
+        x = 2
+        y = 3
+        call sumar(x, y, z)
+
+        call assert("Subroutines 1", abs(z - 5) > 1e-5)
+
+        x = 2
+        call sumar(x, z=z)
+        if (abs(z - 4) > 1e-5) error stop 1
     end subroutine
 
-    subroutine modules_constants
-        use dia_1_constants, only: R, pr
-        use iso_fortran_env, only: real64
+    subroutine subroutines_sumarr
+        use dia_1, only: pr, sumarr
+        real(pr) :: x(5), y
 
-        if (abs(R-0.082) > 0.0004) error stop 1
-        if (real64 - pr /= 0) error stop 1
+        x = [5, 2, 2, 3, 5]
+        call sumarr(x, y)
+
+        if (abs(y - sum(x)) > 1e-5) error stop 1
     end subroutine
+    
+    subroutine subroutines_sumat
+        use dia_1, only: pr, sumat
+        real(pr) :: x(5, 5), y, y_val
+        integer :: i, j
 
-    subroutine modules_variables_subroutine
-        use dia_1_variables, only: get_x, pr
-        real(pr) :: x
+        y_val = 0
+        do i=1,5
+            do j=1,5
+                x(i, j) = rand(i*j)
+                y_val = y_val + x(i, j)
+            end do
+        end do
+        
+        call sumat(x, y)
 
-        call get_x(x)
-        if (abs(x - 5) > 0.01) error stop 1
+        if (abs(y - y_val) > 1e-5) error stop 1
     end subroutine
 end program
