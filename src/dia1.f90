e6faf60c74c31851a639a9558d56e6df7b41356c
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-04 10:46:15 -0300

    dt

--- src/dia1.f90
+++ src/dia1.f90
@@ -26,6 +26,8 @@ module dia_1_subroutines
 
 
    interface suma
+      !! Interface que permite que el compilador detecte automaticametne cual
+      !! de las tres subrutinas llamar.
       module procedure :: sumar, sumarr, sumat
    end interface suma
 
@@ -58,23 +60,60 @@ contains
    end subroutine sumat
 end module dia_1_subroutines
 
+module dia_1_derivados
+   use dia_1_constants, only: pr
+   implicit none
+
+   type :: Margules
+      real(pr) :: A12, A21
+      real(pr) :: Psat(2)
+   contains
+      procedure :: lngamma => ln_gamma
+   end type Margules
+
+contains
+   subroutine ln_gamma(model, x, lngamma)
+      class(Margules), intent(in) :: model
+      real(pr), intent(in) :: x(2)
+      real(pr), intent(out) :: lngamma(2)
+
+      ! Associate nos permite evitar escribri tanto choclo
+      associate(A12 => model%A12, A21 => model%A21)
+         lngamma(1) = (A12 + 2 * (A21 - A12)*x(1))*x(2)**2
+         lngamma(2) = (A21 + 2 * (A12 - A21)*x(2))*x(1)**2
+      end associate
+   end subroutine ln_gamma
+
+   subroutine pressure(model, x, P)
+      class(Margules), intent(in) :: model
+      real(pr), intent(in) :: x(2)
+      real(pr), intent(out) :: P
+
+      real(pr) :: lngammas(2), gammas(2)
+      call model%lngamma(x, lngammas)
+
+      gammas = exp(gammas)
+      P = sum(x * gammas * model%Psat)
+   end subroutine pressure
+end module dia_1_derivados
+
 module dia_1_allocatables
    use dia_1_constants, only: pr
    implicit none
 contains
    subroutine set_dim(x, dim)
       real(pr), allocatable, intent(in out):: x(:)
-      integer, intent(in) ::dim 
+      integer, intent(in) ::dim
       if (allocated(x)) deallocate(x)
       allocate(x(dim))
-   end subroutine
+   end subroutine set_dim
 
    subroutine agrandar(array, nuevo_valor)
       real(pr), allocatable, intent(in out) :: array(:)
       real(pr), intent(in) :: nuevo_valor
       array = [array, nuevo_valor]
-   end subroutine
-end module
+   end subroutine agrandar
+end module dia_1_allocatables
 
 module dia_1
    use dia_1_constants
